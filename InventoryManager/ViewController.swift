81c8b8ddc1c245bb5ab6330f441475398a647917
Alex Grimes <alexkgrimes@gmail.com>
2019-11-24 20:41:33 -0500

    Initial Commit

---
+++ InventoryManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InventoryManager
+//
+//  Created by Alex Grimes on 11/24/19.
+//  Copyright © 2019 Alex Grimes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
